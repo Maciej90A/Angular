a09eb4a914f996537dae00c80aa3e844167d5e50
maciek <augustyniak.maciej1990@gmail.com>
2020-10-03 14:30:38 +0200

    zadanie 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import {Component, OnInit} from '@angular/core';
+import {MoviesServiceService} from './movies-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MoviesServiceService]
 })
 export class AppComponent implements OnInit {
-  isDark = false;
+  private service: MoviesServiceService;
+  topMovies: any;
+  bottomMovies: any;
+
+  constructor(service: MoviesServiceService) {
+    this.service = service;
+  }
 
   ngOnInit(): void {
-  }
-
-  changeStyle(): void {
-    this.isDark = !this.isDark;
+    this.topMovies = this.service.getTopMovies();
+    this.bottomMovies = this.service.getBottomMovies();
   }
 }
-
-
