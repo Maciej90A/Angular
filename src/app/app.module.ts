c2f105facdb01cfd8ab7b2e6bbb330face62bd35
maciek <augustyniak.maciej1990@gmail.com>
2020-10-04 14:01:23 +0200

    zadanie 21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,59 @@ import {MyDirectiveDirective} from './my-directive.directive';
 import {MyPipePipe} from './my-pipe.pipe';
 import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {NewsComponent} from './news/news.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {RouterModule, Routes} from '@angular/router';
+import {APIResolver} from './resolver/APIResolver';
+
+const appRotes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    resolve: {message: APIResolver}
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {page: 'Home page'}
+  },
+  {
+    path: 'news/:newsId',
+    component: NewsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MyDirectiveDirective,
     MyPipePipe,
+    HomeComponent,
+    LoginComponent,
+    NewsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RouterModule.forRoot(appRotes)
   ],
-  providers: [],
+  providers: [
+    APIResolver
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  data: any;
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.data = this.route.snapshot.data;
+  }
+
+}
